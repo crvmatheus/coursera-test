2c21249d2d10b4b1691acdf77377d9105e756a72
C5D8 <caio.rabello@petrobras.com.br>
2022-04-08 13:53:37 -0300

    Again

--- module4-solution/script.js
+++ module4-solution/script.js
@@ -39,7 +39,7 @@ WARNING!!! WARNING!!!
   // Loop over the names array and say either 'Hello' or "Good Bye"
   // using either the helloSpeaker's or byeSpeaker's 'speak' method.
   // See Lecture 50, part 1
-  for (i=0; i < length(names); i++) {
+  for (i=0; i < names.length; i++) {
 
     // STEP 11:
     // Retrieve the first letter of the current name in the loop.
